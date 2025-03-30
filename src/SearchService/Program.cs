d074498566c72ab9241dd6380b3d4d38225e3c13
Sanjay Kothari <skothari120790@gmail.com>
2025-03-30 10:25:38 +0530

    Added swagger in search service

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,6 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Microsoft.OpenApi.Models;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,12 +11,24 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Search Service API", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Search Service API v1");
+    });
 }
 
 app.UseHttpsRedirection();
